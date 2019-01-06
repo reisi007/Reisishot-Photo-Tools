ebdb87c053c2754ac9a3564e7d362c1f663862ba
Florian Reisinger <reisi007@gmail.com>
2019-01-06 21:04:05 +0100

    Updated Kotlin Runtime version

--- performAction.cmd
+++ performAction.cmd
@@ -1,2 +1,2 @@
-REM java -ea -Xss1024m -Xms256m -Xmx8g -jar ./Watermarking/build/libs/Watermarking-1.0-SNAPSHOT-all.jar --settings "D:\_Files\Dropbox\ReisiShot\wmsettings.json"
-java -ea -Xss1024m -Xms256m -Xmx8g -jar ./InstaResize/build/libs/InstaResize-1.0-SNAPSHOT-all.jar --src "D:\Desktop\out" -c BLACK
+java -ea -Xss1024m -Xms256m -Xmx8g -jar ./Watermarking/build/libs/Watermarking-1.0-SNAPSHOT-all.jar --settings "D:\_Files\Dropbox\ReisiShot\wmsettings.json"
+java -ea -Xss1024m -Xms256m -Xmx8g -jar ./InstaResize/build/libs/InstaResize-1.0-SNAPSHOT-all.jar --src "D:\Desktop\out" --out "D:\Desktop\igout" -c WHITE
